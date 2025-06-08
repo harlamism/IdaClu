99a4275d994b48dd82c37341620b8fea43e65b9d
Sergejs Harlamovs <33154977+harlamism@users.noreply.github.com>
2025-06-08 16:57:39 +0200

    :ok_hand: IMPROVE: add explicit func filtering

--- idaclu/plugins/group_6/plugin_1.py
+++ idaclu/plugins/group_6/plugin_1.py
@@ -6,13 +6,22 @@ import idautils
 import idaapi
 #
 from idaclu import ida_shims
+from idaclu import ida_utils
 from idaclu.qt_utils import i18n
 
 
 SCRIPT_NAME = i18n('Explicit Calls')
 SCRIPT_TYPE = 'func'
 SCRIPT_VIEW = 'tree'
-SCRIPT_ARGS = []
+SCRIPT_ARGS = [(
+    'checkbox', 'func_calls',
+    [
+        'Type Casting',
+        'Helper Functions',
+        'Standard Functions',
+        'Custom Functions',
+    ]
+)]
 
 
 def order_item_len(input_dict):
@@ -52,6 +61,11 @@ def get_data(func_gen=None, env_desc=None, plug_params=None):
         'data': collections.defaultdict(list),
         'stat': collections.defaultdict(int)
     }
+    
+    is_typ_add = plug_params['func_calls'][0][1]
+    is_hlp_add = plug_params['func_calls'][1][1]
+    is_std_add = plug_params['func_calls'][2][1]
+    is_usr_add = plug_params['func_calls'][3][1]
 
     for func_addr in func_gen():
         caller_name = idaapi.get_func_name(func_addr)
@@ -60,9 +74,16 @@ def get_data(func_gen=None, env_desc=None, plug_params=None):
         for psdo_line in caller_psdo:
             is_func_matched = re.match('(?:(?:.*\s)?)([0-9a-zA-Z\_\:]+)\(.*\)(?:(?:.*)?)', psdo_line)  # (?:(?:.*\s)?)([0-9a-zA-Z\_\:]+)\(.*\)
             if is_func_matched:
+                
                 calee_name = is_func_matched.group(1)
-                report['data'][calee_name].append((func_addr, psdo_line))
-                report['stat'][calee_name] += 1
+                fn_type = ida_utils.get_fn_type(calee_name)
+                if ((is_typ_add and fn_type == 'typ')
+                    or (is_hlp_add and fn_type == 'hlp')
+                    or (is_std_add and fn_type == 'std')
+                    or (is_usr_add and fn_type == 'usr')
+                   ):
+                    report['data'][calee_name].append((func_addr, psdo_line))
+                    report['stat'][calee_name] += 1
 
     report['data'] = order_item_len(report['data'])
     report['stat'] = order_item_len(report['stat'])
